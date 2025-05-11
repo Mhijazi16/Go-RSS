82f3e055bab1ed4041a73e350bbd432209377f29
mhijazi16 <gultimate0@gmail.com>
2025-05-11 20:52:52 +0300

    getting env variables

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mhijazi16/Go-RSS
 
 go 1.24.3
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("initial commit")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		log.Fatal("make sure you set PORT in .env file.")
+	}
+
+	fmt.Println(PORT)
 }
