c483c8ed9597f6da7d1e457d9988afa570924adf
mhijazi16 <gultimate0@gmail.com>
2025-05-13 21:10:05 +0300

    adding simple health-check api

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"github.com/joho/godotenv"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
-func main() {
+type response struct {
+	Status string
+}
 
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	var data = response{Status: "success"}
+	var res, err = json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(res)
+}
+
+func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error loading .env file:", err)
 	}
 
 	PORT := os.Getenv("PORT")
@@ -19,5 +38,11 @@ func main() {
 		log.Fatal("make sure you set PORT in .env file.")
 	}
 
-	fmt.Println(PORT)
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/health-check", handleHome)
+
+	log.Printf("Starting server on port %s...\n", PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, router))
 }
