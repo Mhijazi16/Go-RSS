a7fbd0875b231bc69f1a463613166e011f9fbd16
mhijazi16 <gultimate0@gmail.com>
2025-05-16 20:27:20 +0300

    adding more queries

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -13,9 +13,11 @@ import (
 )
 
 const createUser = `-- name: CreateUser :one
-INSERT INTO users (id, name, password, created_at, updated_at)
-VALUES ($1, $2, $3, $4, $5)
-RETURNING id, name, password, created_at, updated_at
+INSERT INTO users (id, name, password, created_at, updated_at, api_key)
+VALUES ($1, $2, $3, $4, $5, 
+  encode(sha256(random()::text::bytea), 'hex')
+)
+RETURNING id, name, password, created_at, updated_at, api_key
 `
 
 type CreateUserParams struct {
@@ -41,6 +43,25 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 		&i.Password,
 		&i.CreatedAt,
 		&i.UpdatedAt,
+		&i.ApiKey,
+	)
+	return i, err
+}
+
+const getUserByAPIKey = `-- name: GetUserByAPIKey :one
+SELECT id, name, password, created_at, updated_at, api_key FROM users WHERE api_key = $1
+`
+
+func (q *Queries) GetUserByAPIKey(ctx context.Context, apiKey string) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUserByAPIKey, apiKey)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.Name,
+		&i.Password,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.ApiKey,
 	)
 	return i, err
 }
